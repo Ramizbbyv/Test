78ee3db9e74507c118bd8aff2999824cea48251a
Ramiz Babayev <rbabayev349@gmail.com>
2022-09-23 16:45:46 +0400

    "New files added"

--- src/main/kotlin/Null safety.kt
+++ src/main/kotlin/Null safety.kt
@@ -1,20 +1,47 @@
 fun main() {
-     var str:String? = "null safety lessons"
-   //// str = null
-   // println(str!!.length)
-    /*null olabilecek bir hatadan kurtulak icin  if den use ede bilerik
+     //   var str:String? = "null safety lessons"
+     //// str = null
+     // println(str!!.length)
+     /*null olabilecek bir hatadan kurtulak icin  if den use ede bilerik
     Elvis operatoru - ---?:----
 
      */
-   // if (str !=null){
-   //     println(str.length)
-   // }
-   // else {
-   //     println("-1")
-   // }
-   // str = null
-   // var length:Int = str?.length ?: -1
-   // println(length)
-
+     // if (str !=null){
+     //     println(str.length)
+     // }
+     // else {
+     //     println("-1")
+     // }
+     // str = null
+     // var length:Int = str?.length ?: -1
+     // println(length)
+     var i = 1
+     var cem = 1
+     while (i < 20) {
+          cem -= i
+          i++
+     }
+    // println("Cavab = " + cem)
+    var a =  multiply(4)
+     println(a)
+    var d =  5.multi()
+     println(d)
+     var e1 = "Ramiz" ekle " " +
+             "Babayev"
+     println(e1)
+}
+fun multiply(x1:Int):Int{
+     return 4*x1
 
 }
+fun Int.multi():Int{
+     return this*9
+}
+
+
+infix fun String.ekle(deger:String):String{
+     return this+deger
+}
+
+
+
